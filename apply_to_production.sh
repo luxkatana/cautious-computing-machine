7a79e8cd5aa84e0c3349b2378b505b934808d3a3
luxkatana <taseen.bibi@gmail.com>
2025-01-19 19:23:13 +0000

    apply_to_production.sh change

--- apply_to_production.sh
+++ apply_to_production.sh
@@ -2,7 +2,7 @@
 git pull
 
 echo "Stopping unit"
-systemctl --user stop event
+systemctl --user kill -s SIGKILL event
 echo "Starting unit"
 systemctl --user start event
 echo "Successfully Started!"
